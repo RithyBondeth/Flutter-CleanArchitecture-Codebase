1f1816cecb8c4da4912a757300cdbc5195bfef2f
Rithy Bondeth <rithybondeth999@gmail.com>
2026-02-21 13:34:54 +0700

    implement auto route

--- lib/features/posts/presentation/screens/main_post_screen.dart
+++ lib/features/posts/presentation/screens/main_post_screen.dart
@@ -1,6 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+@RoutePage(name: 'MainPostScreen')
 class MainPostScreen extends ConsumerWidget {
   const MainPostScreen({super.key});
 
